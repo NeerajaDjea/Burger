b79fb7dc8b77a70dbea0da1ba1ce8867fbaaab26
Neeraja Djearaman <neeru.jai@gmail.com>
2020-01-11 18:12:16 +0000

    add routes

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,2 +1,50 @@
-INSERT INTO burgers(burger_name)
-VALUES('Double Burger'), ('Cheeseburger'), ('Chicken Burger'), ('Ham Burger');
+var express = require("express");
+var router = express.Router();
+
+// Import the model (burger.js) to use its database functions.
+var burger = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/", function(req, res) {
+    burger.create([
+            "burger_name", "devoured"
+        ], [
+            req.body.burger_name, req.body.devoured
+        ],
+        function() {
+            res.redirect("/");
+        });
+});
+
+router.put("/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function() {
+        res.redirect("/");
+    });
+});
+
+router.delete("/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    burger.delete(condition, function() {
+        res.redirect("/");
+    });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,26 @@
+var connection = require("./config/connection");
+var express = require("express");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+var routes = require("./controllers/burger_controller.js")
+var app = express();
+var methride = require("method-override");
+
+// use the routes
+app.use(routes)
+    // for deploying on heroku
+var PORT = process.env.PORT || 3006;
+//  bodyParser for the urlencoded and json
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// set up handlebars for layout
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function() {
+    console.log("server listening on http://localhost" + PORT);
+
+});
