d5563b1eccc40d879f48554c62c3cc00718439cb
Neeraja Djearaman <neeru.jai@gmail.com>
2020-02-02 18:28:17 +0000

    changed config file to enable heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require("mysql");
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
 
-var connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "MyNewPass",
-    database: "burgers_db"
-});
+    var connection = mysql.createConnection({
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "MyNewPass",
+        database: "burgers_db"
+    });
+};
+
+
 
 // Make connection.
 connection.connect(function(err) {
